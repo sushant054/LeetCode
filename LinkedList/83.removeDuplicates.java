462c1300998fbd10503f38e0038d0c081bca52c2
sushant054 <sushantnalage54@gmail.com>
2024-07-15 22:49:44 +0530

    remove duplicates from sorted list

---
+++ LinkedList/83.removeDuplicates.java
@@ -0,0 +1,22 @@
+package LinkedList;
+class removeDuplicates {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head == null){
+            return null;
+        }
+        ListNode prev = head;
+        ListNode temp = prev.next;
+        while(temp != null){
+            if(prev.val == temp.val){
+                temp = temp.next;
+                continue;
+            }
+            prev.next=temp;
+            prev=temp;
+            temp=temp.next;
+
+        }
+        prev.next=null;
+        return head;
+    }
+}
